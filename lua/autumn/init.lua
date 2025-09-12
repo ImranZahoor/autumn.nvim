5ae0de78f65c06b37742268089affa694ddff867
Imran Zahoor <imran-zahoor@outlook.com>
2025-09-13 02:30:16 +0500

    adds lualine theme module

--- lua/autumn/init.lua
+++ lua/autumn/init.lua
@@ -4,8 +4,5 @@ function M.setup()
 	for group, opts in pairs(theme.highlights) do
 		vim.api.nvim_set_hl(0, group, opts)
 	end
-
-	-- auto configure color if lualine is installed
-	package.loaded["lualine.themes.autumn"] = require("autumn.lualine")
 end
 return M

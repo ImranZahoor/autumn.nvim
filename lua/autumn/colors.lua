b43ab184363e494150b4bb416b18799be9677283
Imran Zahoor <imran-zahoor@outlook.com>
2025-09-13 01:53:22 +0500

    adds colors file

---
+++ lua/autumn/colors.lua
@@ -0,0 +1,15 @@
+-- autumn/colors.lua
+local colors = {
+	brown_light = "#8A6553",
+	brown_medium = "#79453E",
+	deep_purple = "#110A0E",
+	black_deep = "#030001",
+	gray_purple = "#524A4E",
+	taupe = "#875E4A",
+	charcoal = "#403E49",
+	orange_burnt = "#CC4103",
+	red_bright = "#FF021A",
+	red_wine = "#951537",
+}
+
+return colors
